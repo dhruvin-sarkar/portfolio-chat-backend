7afe08804f32beff1145b58803f40ec442570ab5
Dhruvin Sarkar <dhruvinsarkar@outlook.com>
2026-03-10 15:55:37 +0400

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors({
   origin: [
     "http://localhost:3000",
+    "http://localhost:5173",
     "https://dhruvin-sarkar-dev.vercel.app",
     "https://dhruvin-sarkar.dev",
     "https://www.dhruvin-sarkar.dev"
